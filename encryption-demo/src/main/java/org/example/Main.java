a191bd1f307c3a5d3ea842026b2ffc6177f17929
cherry <1466947023@qq.com>
2024-01-28 09:36:29 +0800

    加密项目

---
+++ encryption-demo/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+/**
+ * encryption-demo
+ *
+ * @Description: TODO
+ * @Author: cherry
+ * @Create on: 2024/1/28
+ **/
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class);
+    }
+}
